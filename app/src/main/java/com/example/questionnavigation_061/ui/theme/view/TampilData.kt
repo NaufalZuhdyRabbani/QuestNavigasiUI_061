3305a6b8d96cf4fa45331d173a6a4d895d196add
NaufalZuhdyRabbani <naufalkzrz@gmail.com>
2025-11-06 14:07:20 +0700

    MEMPERBAIKI ERROR DI TAMPIL DATA

--- app/src/main/java/com/example/questionnavigation_061/ui/theme/view/TampilData.kt
+++ app/src/main/java/com/example/questionnavigation_061/ui/theme/view/TampilData.kt
@@ -1,12 +1,14 @@
-package com.example.navigasiui.view
+package com.example.navigasilayout.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -24,44 +26,56 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.questionnavigation_061.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     onBackBtnClick:()-> Unit
-){
+) {
     val items = listOf(
         Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
         Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
-        Pair(stringResource(id = R.string.alamat), "Yogyakarta")
-    )
-    Scaffold (modifier = Modifier,
+        Pair(stringResource(id = R.string.alamat), "Yogyakarta"),
+
+        )
+    Scaffold(
+        modifier = Modifier,
         {
             TopAppBar(
-                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White)},
-                colors = TopAppBarDefaults
-                    .mediumTopAppBarColors(colorResource(id=R.color.teal_700))
+                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.teal_700))
             )
-        }){ isiRuang->
-        Column(modifier = Modifier.padding(isiRuang),
-            verticalArrangement = Arrangement.SpaceBetween) {
-            Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
-                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))) {
+        }) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+            ) {
                 items.forEach { item ->
                     Column {
-                        Text(text = item.first.uppercase(),
-                            fontSize = 16.sp)
-                        Text(text = item.second,
+                        Text(
+                            text = item.first.uppercase(),
+                            fontSize = 16.sp
+                        )
+                        Text(
+                            text = item.second,
                             fontWeight = FontWeight.Bold,
-                            fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                            fontFamily = FontFamily.Cursive, fontSize = 22.sp
+                        )
                     }
                     HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
                 }
-                Spacer(modifier = Modifier.height(height = 10.dp))
+                Spacer(modifier = Modifier.height(10.dp))
                 Button(
                     modifier = Modifier.fillMaxWidth(),
-                    onClick = onBackBtnClick) {
-                    Text(text = stringResource(id=R.string.back))
+                    onClick = onBackBtnClick
+                ) {
+                    Text(text = stringResource(id = R.string.back))
                 }
+
             }
         }
-        }
+    }
 }
